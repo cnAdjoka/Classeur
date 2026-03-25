c3b9398bc2f2b05cafb4d6c978e1dd997bc5f1fd
cnAdjoka <ragequit078@gmail.com>
2026-03-25 01:00:17 -0400

    wtv

--- src/main/java/com/maintenanceAdjoka/app/backend/models/Client.java
+++ src/main/java/com/maintenanceAdjoka/app/backend/models/Client.java
@@ -5,12 +5,15 @@ public class Client{
   private String name;
   private String phoneNumber;
   private String address;
-
+  private int seasonPrice;
+  private int remainingDebt;
 
 public Client (String name, String phoneNumber, String address){
   this.name = name;
   this.phoneNumber = phoneNumber;
   this.address = address;
+  this.seasonPrice = 0;
+  this.remainingDebt = seasonPrice;
 }
 
 public void setName(String newName){
@@ -37,4 +40,32 @@ public void setAddress(String newAddress){
 public String getAddress(){
   return this.address;
 }
+
+public void setSeasonPrice(int amount){
+  this.seasonPrice =  amount;
+}
+
+public int getSeasonPrice(){
+  return this.seasonPrice;
+}
+
+public void printBalance(){
+  int amountPayed = seasonPrice - remainingDebt;
+
+  if(this.seasonPrice != 0){
+
+    System.out.println("Initial Amount : " + seasonPrice);
+    System.out.println("Amount Payed : " + amountPayed);
+    System.out.println("Amount Left to Pay :" + remainingDebt);
+  }
+
+  else{System.out.println("Client owes nothing");}
+}
+
+public void pay(int amountPayed){
+  this.remainingDebt = remainingDebt - amountPayed;
+
+}
+
+
 }
